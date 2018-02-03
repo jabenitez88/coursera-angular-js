42e35db2c8599e94b0b0a9cd84ff94d7a2d63243
Jose Alberto <jabenitez@gmail.com>
2018-02-03 13:35:26 +0100

    Module 2 solution fixed

--- module2-solution/js/app.js
+++ module2-solution/js/app.js
@@ -1,21 +1,10 @@
 (function () {
 'use strict';
-//Declare `ng-app` either on the `html` or the `body` element.
-//Name your app `ShoppingListCheckOff`.
+
 
 angular.module('ShoppingListCheckOff', [])
-
-//Go back to `index.html` and declare 2 controllers using `controller as` syntax. One controller should be called `ToBuyShoppingController` and the other called `AlreadyBoughtShoppingController`.
-//You are *required* to have 2 controllers for this assignment.
-//
-
 .controller('ToBuyShoppingController',ToBuyShoppingController)
 .controller('AlreadyBoughtShoppingController',AlreadyBoughtShoppingController)
-
-//You will obviously need to share data between these controllers.
-//Go back to `app.js` and implement this data sharing using the *singleton* approach with the `.service` declaration.
-//Call the service `ShoppingListCheckOffService`.
-
 .service('ShoppingListCheckOffService',ShoppingListCheckOffService);
 
   //1 array
@@ -45,29 +34,25 @@ angular.module('ShoppingListCheckOff', [])
 
     var toBuyFoods =
     [
-     {name: 'bananas', quantity: 4},
-     {name: 'honey', quantity: 1},
-     {name: 'apples', quantity: 2},
-     {name: 'coffee', quantity: 1},
+     {name: 'bananas', quantity: 1},
+     {name: 'honey', quantity: 5},
+     {name: 'apples', quantity: 9},
+     {name: 'coffee', quantity: 2},
      {name: 'grapes', quantity: 3},
      {name: 'potatoes', quantity: 5},
      {name: 'milk', quantity: 3}
     ];
 
-
     var toBoughtFoods = [];
-
     service.removeAndAddtoBought = function (FoodIndex)
     {
       var ItemFoods = toBuyFoods.splice(FoodIndex, 1);
       toBoughtFoods.push(ItemFoods[0]);
     };
-
     service.getToBuyFoods = function ()
     {
       return toBuyFoods;
     };
-
     service.getToBoughtFoods = function ()
     {
       return toBoughtFoods;
